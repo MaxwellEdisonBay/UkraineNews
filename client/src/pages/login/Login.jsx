cdaba1688434f55f611042fe6c2e973055bee05e
Ivan <ivan.teslenko@cleverlance.com>
2022-10-01 03:15:52 -0400

    Login form temporarily removed

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -4,27 +4,8 @@ import React from "react";
 export default function Login() {
   return (
     <div className="login">
-      <span className="login-title">Login</span>
-
-      <div id="signInDivLogin" />
-      <form className="login-form">
-        <label>Email</label>
-        <input
-          type="text"
-          className="login-input"
-          placeholder="Enter your email"
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          className="login-input"
-          placeholder="Enter your password"
-        />
-        <div className="login-buttons-bar">
-          <button className="login-button">Login</button>
-          <button className="login-register-button">Sign Up</button>
-        </div>
-      </form>
+      <span className="login-title">Přihláste se přes Google pro pokračování</span>
+      
     </div>
   );
 }
